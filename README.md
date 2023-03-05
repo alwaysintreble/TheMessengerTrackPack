236a3028bbbeed1c009d7422ae88b93e4e484739
alwaysintreble <mmmcheese158@gmail.com>
2023-03-05 11:32:34 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# The Messenger Randomizer Track Pack
+
+## Quick Links
+- [Archipelago](https://archipelago.gg/)
+- [Archipelago Game Info Page](https://archipelago.gg/games/The%20Messenger/info/en)
+- [Courier](https://github.com/Brokemia/Courier)
+- [The Messenger Randomizer Github](https://github.com/minous27/TheMessengerRandomizerMod)
+- [Jacksonbird8237's HTML Item Tracker](https://github.com/Jacksonbird8237/TheMessengerItemTracker)
+- [Releases]()
+
+## About
+This is the Poptracker Pack for The Messenger Randomizer and supports Archipelago Autotracking.
+
+## Requirements
+[PopTracker](https://github.com/black-sliver/PopTracker)
+
+## Installation
+
+1. Download the latest release
+2. Place the downloaded zip file in the PopTracker packs directory.
+
+## Usage
+The tracker will show you what you can access with your current items. Glitch/Trick logic is not currently supported. For Archipelago
+Autotracking details see PopTracker's README.
---
+++ The Messenger Track Pack/scripts/autotracking/archipelago.lua
@@ -0,0 +1,149 @@
+ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
+ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
+
+CUR_INDEX = -1
+SLOT_DATA = nil
+LOCAL_ITEMS = {}
+GLOBAL_ITEMS = {}
+
+function onClear(slot_data)
+    if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+        print(string.format("called onClear, slot_data:\n%s", dump_table(slot_data)))
+    end
+    SLOT_DATA = slot_data
+    CUR_INDEX = -1
+    -- reset locations
+    for _, v in pairs(LOCATIONS_MAPPING) do
+        if v[1] then
+            if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+                print(string.format("onClear: clearing location %s", v[1]))
+            end
+            local obj = Tracker:FindObjectForCode(v[1])
+            if obj then
+                if v[1]:sub(1, 1) == "@" then
+                    obj.AvailableChestCount = obj.ChestCount
+                else
+                    obj.Active = false
+                end
+            elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+                print(string.format("onClear: could not find object for code %s", v[1]))
+            end
+        end
+    end
+    -- reset items
+    for _, v in pairs(ITEM_MAPPING) do
+        if v[1] and v[2] then
+            if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+                print(string.format("onClear: clearing item %s of type %s", v[1], v[2]))
+            end
+            local obj = Tracker:FindObjectForCode(v[1])
+            if obj then
+                if v[2] == "toggle" then
+                    obj.Active = false
+                elseif v[2] == "progressive" then
+                    obj.CurrentStage = 0
+                    obj.Active = false
+                elseif v[2] == "consumable" then
+                    obj.AcquiredCount = 0
+                elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+                    print(string.format("onClear: unknown item type %s for code %s", v[2], v[1]))
+                end
+            elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+                print(string.format("onClear: could not find object for code %s", v[1]))
+            end
+        end
+    end
+    LOCAL_ITEMS = {}
+    GLOBAL_ITEMS = {}
+    -- update slot based info
+    if ENABLE_DEBUG_LOG then
+        print("Checking slot data.")
+        print(string.format("goal: %s", slot_data["goal"]))
+        print(string.format("required seals: %s", slot_data["required_seals"]))
+    end
+    if slot_data["goal"] then
+        if slot_data["goal"] == "open_music_box" then
+            Tracker:FindObjectForCode("goal").CurrentStage = 0
+        end
+        if slot_data["goal"] == "power_seal_hunt" then
+            Tracker:FindObjectForCode("goal").CurrentStage = 1
+            Tracker:FindObjectForCode("required_seals").AcquiredCount = tonumber(slot_data["required_seals"])
+        end
+    end
+    
+    if slot_data["settings"] then
+        if slot_data["settings"]["Difficulty"] == "Basic" then
+            Tracker:FindObjectForCode("shuffled_power_seals").CurrentStage = 0
+        end
+    end
+end
+
+function onItem(index, item_id, item_name, player_number)
+    if index <= CUR_INDEX then
+        return
+    end
+    local is_local = player_number == Archipelago.PlayerNumber
+    CUR_INDEX = index;
+    local v = ITEM_MAPPING[item_id]
+    if not v then
+        if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+            print(string.format("onItem: could not find item mapping for id %s", item_id))
+        end
+        return
+    end
+    if not v[1] then
+        return
+    end
+    local obj = Tracker:FindObjectForCode(v[1])
+    if obj then
+        if v[2] == "toggle" then
+            obj.Active = true
+        elseif v[2] == "progressive" then
+            if obj.Active then
+                obj.CurrentStage = obj.CurrentStage + 1
+            else
+                obj.Active = true
+            end
+        elseif v[2] == "consumable" then
+            obj.AcquiredCount = obj.AcquiredCount + obj.Increment
+        elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
+            print(string.format("onItem: unknown item type %s for code %s", v[2], v[1]))
+        end
+    end
+
+    if is_local then
+        if LOCAL_ITEMS[v[1]] then
+            LOCAL_ITEMS[v[1]] = GLOBAL_ITEMS[v[1]] + 1
+        else
+            LOCAL_ITEMS[v[1]] = 1
+        end
+    else
+        if GLOBAL_ITEMS[v[1]] then
+            GLOBAL_ITEMS[v[1]] = GLOBAL_ITEMS[v[1]] + 1
+        else
+            GLOBAL_ITEMS[v[1]] = 1
+        end
+    end
+end
+
+function onLocation(location_id, location_name)
+    local v = LOCATIONS_MAPPING[location_id]
+    local d = lOCATIONS_MAPPING[location_id]
+    if not v[1] then
+        return
+    end
+    local obj = Tracker:FindObjectForCode(v[1])
+    local detObj = Tracker:FindObjectForCode(d[1])
+    if obj then
+        if v[1]:sub(1, 1) == "@" then
+            obj.AvailableChestCount = obj.AvailableChestCount - 1
+            detObj.AvailableChestCount = detObj.AvailableChestCount - 1
+        else
+            obj.Active = true
+            detObj.Active = true
+        end
+    end
+end
+Archipelago:AddClearHandler("clear handler", onClear)
+Archipelago:AddItemHandler("item handler", onItem)
+Archipelago:AddLocationHandler("location handler", onLocation)
